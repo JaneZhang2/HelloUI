e713f3dc4a12ed464b314f53353353058ab70b73
张静 <jane@bigsec.com>
2017-05-10 18:22:23 +0800

    normalize.scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const PATHS = {
   src: path.join(__dirname, 'src'),
   dist: path.join(__dirname, 'dist')
 };
+const ExtractNormalizeCSSPlugin = new ExtractTextPlugin({filename: 'normalize.scss'});
 
 module.exports = {
   entry: [PATHS.src],
@@ -16,11 +18,13 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
+        include: path.join(__dirname, 'node_modules/normalize.css'),
+        use: ExtractNormalizeCSSPlugin.extract(['css-loader'])
+      }
+    ]
   },
   plugins: [
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin(),
+    ExtractNormalizeCSSPlugin
   ]
 };
